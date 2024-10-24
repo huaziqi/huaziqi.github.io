6a3e674fe52299de138cb67ab14a64336208f6f0
huaziqi <2946048113@qq.com>
2024-10-25 00:15:25 +0800

    Site updated: 2024-10-25 00:15:24

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const timeConsumed = document.getElementById('time-consumed');
+let nowDate = new Date();
+function updateTime() {
+    timeConsumed.innerHTML = (Date(nowDate.getTime(), "YYYY-MM-DD HH:mm:ss"));
+    nowDate = new Date();
+}
+updateTime();
+setInterval(updateTime, 1000);
