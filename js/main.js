f8ba9bbdb19f921fd7a18c08dc4bd4cbe92de00a
huaziqi <2946048113@qq.com>
2024-10-29 00:22:56 +0800

    Site updated: 2024-10-29 00:22:55

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const timeConsumed = document.getElementById('time-consumed');
-let nowDate = new Date();
-function updateTime() {
-    timeConsumed.innerHTML = (Date(nowDate.getTime(), "YYYY-MM-DD HH:mm:ss"));
-    nowDate = new Date();
-}
-updateTime();
-setInterval(updateTime, 1000);
+
+
+
+// const timeConsumed = document.getElementById('time-consumed');
+// let nowDate = new Date();
+// function updateTime() {
+//     timeConsumed.innerHTML = (Date(nowDate.getTime(), "YYYY-MM-DD HH:mm:ss"));
+//     nowDate = new Date();
+// }
+// updateTime();
+// setInterval(updateTime, 1000);
+
