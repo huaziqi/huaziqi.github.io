c9eeefe3305a9fce83595d1ca6be7dd463787ae5
huaziqi <2946048113@qq.com>
2024-11-20 21:15:40 +0800

    Site updated: 2024-11-20 21:15:39

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-const indexDescription = document.querySelector("#indexDescription span");
-let i = 0;
-let text = "测试test...";
-function updateIndexDescription() {
-    if(i < text.length){
-        indexDescription.innerHTML += text.charAt(i);
-        i ++;
-        setTimeout(updateIndexDescription, 1000);
-    }
-}
+// const indexDescription = document.querySelector("#indexDescription span");
+// let i = 0;
+// let text = "测试test...";
+// function updateIndexDescription() {
+//     if(i < text.length){
+//         indexDescription.innerHTML += text.charAt(i);
+//         i ++;
+//         setTimeout(updateIndexDescription, 1000);
+//     }
+// }
 
-updateIndexDescription();
+// updateIndexDescription();
